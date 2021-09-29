bc5caef9dd9a49866fecd7ea46e8cc09009de601
Osmar <72116734+ocsj@users.noreply.github.com>
2021-09-29 00:04:38 -0300

    add files

---
+++ 12_Função_Console.log/script.js
@@ -0,0 +1,7 @@
+let idade = 28;
+let nome= "jr";
+
+console.log(idade);
+console.log(nome);
+
+console.log(`o meu nome  é ${nome}, e tenho ${idade} anos`);
---
+++ 14_Condição_IF/script.js
@@ -0,0 +1,21 @@
+let idade = 21
+
+if (idade > 19) {
+    console.log ('a idade é = 19');
+}
+
+if(idade > 25 ) {
+console.log('é maior que 20');
+}
+
+let nome= "Junior";
+
+if (nome == "Junior" && idade > 10){
+    console.log("Liberado");
+}
+
+let passaporte = true;
+
+if ((nome == "Junior" && idade > 10) || true){
+    console.log("Liberado 2");
+}
